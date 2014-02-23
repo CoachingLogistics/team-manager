85c3cf261669b9c1fecd3ab5d3423b576a5d22fe
Alex Egan <aegan@andrew.cmu.edu>
2014-02-22 19:23:24 -0500

    changes to player model

--- app/models/player.js
+++ app/models/player.js
@@ -9,4 +9,28 @@ var PlayerSchema = new Schema({
   date_of_birth: Date
 });
 
+
+// gets the name in last, first format
+PlayerSchema.virtual('official_name').get(function() {
+	return this.last_name + ', ' + this.first_name;
+});
+
+// gets the name in first last format
+PlayerSchema.virtual('full_name').get(function() {
+	return this.first_name + ' ' + this.last_name;
+});
+
+// age
+PlayerSchema.virtual('age').get(function() {
+	var day = this.date_of_birth.getDate();
+	var month = this.date_of_birth.getMonth();
+	var year = this.date_of_birth.getFullYear();
+	var current = new Date();
+	var age = current.getFullYear() - year;
+	if((month > current.getMonth()) || (month == current.getMonth() && day > current.getDate())) {
+		age = age -1;
+	}
+	return age;
+});
+
 mongoose.model('Player', PlayerSchema);
