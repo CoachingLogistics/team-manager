033b5fe056a2b01ea614e6d67ea541d1a30eee17
Alex Egan <aegan@andrew.cmu.edu>
2014-02-27 17:13:44 -0500

    adds mail template model

---
+++ app/models/mail_template.js
@@ -0,0 +1,23 @@
+// Synchronously load model dependecies, so foreign model calls can be made
+var fs = require('fs');
+var models_path = __dirname;
+fs.readdirSync(models_path).forEach(function (file) {
+  if (~file.indexOf('.js')) require(models_path + '/' + file);
+});
+
+// mongoose and model stuff
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var Team = mongoose.model('Team');
+
+// define the schema
+var Email_Template_Schema = new Schema({
+  team_id: {type: ObjectId, required: true},
+  subject: String,
+  recipients: {type: Array, required: true},
+  body: {type: String, required: true}
+});
+
+// and here is the model
+mongoose.model('Email_Template', Email_Template_Schema);
