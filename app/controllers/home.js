ae97de07ddd2dcfd4b41465230afa66e50c4accb
Duan Wang <duanw@andrew.cmu.edu>
2014-04-17 18:05:03 -0400

    removed demos

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,29 +1,17 @@
-var mongoose = require('mongoose'),
-	Coach = mongoose.model('Coach');
+// var mongoose = require('mongoose'),
+//   Article = mongoose.model('Article');
 
-var USER = 'user';
-var COACH = 'coach';
-var role;
 
 exports.index = function(req, res){
-	Coach.getByUserId(req.user._id, function(err, coaches){
-		if(coaches.length == 0){
-			role = USER;
-		}else{
-			role = COACH;
-		}
-	});
-  	res.render('home/index', {
-    	title: 'Team Manager',
-    	user: req.user,
-    	role: role
+  res.render('home/index', {
+    title: 'Generator-Express MVC',
+    user: req.user
   });
 };
 
 exports.err = function(req, res){
   res.render('404', {
-    title: 'Team Manager',
-    user: req.user,
-    role: role
+    title: 'Generator-Express MVC',
+    user: req.user
   });
 };
